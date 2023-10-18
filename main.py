63c2bc1be90021907b53c757b043dba63a9804a1
Dinith Dharshika <dharshikadinith@gmail.com>
2023-10-18 13:07:06 +0530

    updated

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 import PIL
-from fastapi import FastAPI, File, UploadFile
-# from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 from azure.storage.blob import BlobServiceClient
 import h5py
-app = FastAPI()
+from flask import Flask, request, jsonify
 
-# origins = [
-#     "http://localhost",
-#     "http://localhost:3000",
-# ]
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
 global_remedy = ""
+message = ""
 
-connection_string='DefaultEndpointsProtocol=https;AccountName=insectpredct;AccountKey=N9IR0EUF53hmrw3hy5YYVLDTng1GwV5iHWmJqTNM6ql06KM5Y02+8pbd0Cnd70BNBC9EGhkXOFYZ+ASt+56kJg==;EndpointSuffix=core.windows.net'
+connection_string = 'DefaultEndpointsProtocol=https;AccountName=insectpredct;AccountKey=N9IR0EUF53hmrw3hy5YYVLDTng1GwV5iHWmJqTNM6ql06KM5Y02+8pbd0Cnd70BNBC9EGhkXOFYZ+ASt+56kJg==;EndpointSuffix=core.windows.net'
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 container_client = blob_service_client.get_container_client("savedmodels")
 blob_client = container_client.get_blob_client("Insect_bite_model.h5")
@@ -39,8 +29,8 @@ with BytesIO() as f:
 CLASS_NAMES = ['ants', 'bed_bugs', 'mosquito', 'no_bites', 'ticks']
 
 
-@app.get("/ping")
-async def ping():
+@app.route("/ping")
+def ping():
     return "Hello, I am alive"
 
 
@@ -49,13 +39,11 @@ def read_file_as_image(data) -> np.ndarray:
     return image
 
 
-@app.post("/predict")
-async def predict(
-        file: UploadFile = File(...)
-):
+@app.route("/predict", methods=['POST'])
+def predict():
     try:
-
-        image = read_file_as_image(await file.read())
+        file = request.files.get('file')
+        image = read_file_as_image(file.read())
         img_batch = np.expand_dims(image, 0)
 
         predictions = MODEL.predict(img_batch)
@@ -67,44 +55,49 @@ async def predict(
                 'Message': 'Unknown Bite'
             }
         else:
-            return {
-                'Insect': predicted_class,
-                'Message': 'Apply ' + global_remedy + ' onto the affected area.',
-                'confidence': "{0:.0%}".format(float(confidence))
-            }
+            return jsonify(
+                Insect=predicted_class,
+                Message='Apply ' + global_remedy + ' onto the affected area.',
+                confidence="{0:.0%}".format(float(confidence))
+            )
+
+
     except Exception as e:
         error = "Error"
-        return {
-            'response': error
-        }
+        return jsonify(response=error)
 
 
-@app.post("/allergy")
-async def allergy(allergies: str):
+@app.route("/allergy", methods=['POST'])
+def allergy():
+    allergies = request.args.get('allergies')
     global global_remedy
+    global message
     if allergies == "Redness":
         remedy = "Citronella Oil"
         global_remedy = remedy
-        return {"message": "Allergy saved successfully"}
+        message = "Allergy saved successfully"
     elif allergies == "Itching":
         remedy = "Ice/Aloe/Honey/Olive oil"
         global_remedy = remedy
-        return {"message": "Allergy saved successfully"}
+        message = "Allergy saved successfully"
     elif allergies == "Swelling":
         remedy = "Ice"
         global_remedy = remedy
-        return {"message": "Allergy saved successfully"}
+        message = "Allergy saved successfully"
     elif allergies == "Pain":
         remedy = "Ice/Honey"
         global_remedy = remedy
-        return {"message": "Allergy saved successfully"}
+        message = "Allergy saved successfully"
     elif allergies == "Heating":
         remedy = "Ice"
         global_remedy = remedy
-        return {"message": "Allergy saved successfully"}
+        message = "Allergy saved successfully"
     else:
-        return {"message": "Allergy type does not exist"}
+        message = "Allergy type does not exist"
+
+    return jsonify(message=message)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app,host='insect-app.azurewebsites.net')
+    app.debug = True
+    app.run()
